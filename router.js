eea34dee2e2737f7ab4f706c0a6be7b8680fd502
Josh Colvin <joshuacolvin@Joshuas-MacBook-Pro.local>
2022-08-02 14:20:25 -0400

    Completed home page outline and scaffolding

--- router.js
+++ router.js
@@ -6,44 +6,44 @@ const Router = express.Router();
 Router.get("/",(req,res) => {
 	const compiledFile = pug.compileFile("./views/index.pug");
 
-	res.send(compiledFile({ title: "Home", heading: "Welcome to the Mental Health website" }))
+	res.send(compiledFile({ title: "Home" }))
 });
 // Find a professional main link
 Router.get("/find-a-professional",(req,res) => {
 	const compiledFile = pug.compileFile("./views/find-a-professional.pug");
 
-	res.send(compiledFile({ title: "Find a Professional", heading: "Find a Professional!" }));
+	res.send(compiledFile({ title: "Find a Professional" }));
 });
 Router.get("/doctors",(req,res) => {
 	const compiledFile = pug.compileFile("./views/doctors.pug");
 
-	res.send(compiledFile({ title: "Doctor Profiles", heading: "Doctor Profiles" }));
+	res.send(compiledFile({ title: "Doctor Profiles" }));
 });
 Router.get("/book-appointment",(req,res) => {
 	const compiledFile = pug.compileFile("./views/book-appointment.pug");
 
-	res.send(compiledFile({ title: "Book an Appointment", heading: "Book an Appointment with a Professional Today!" }))
+	res.send(compiledFile({ title: "Book an Appointment" }))
 })
 // Forums main link
 Router.get("/forums",(req,res) => {
 	const compiledFile = pug.compileFile("./views/forums.pug");
 
-	res.send(compiledFile({ title: "Forums", heading: "Forums" }));
+	res.send(compiledFile({ title: "Forums" }));
 });
 Router.get("/articles",(req,res) => {
 	const compiledFile = pug.compileFile("./views/articles.pug");
 
-	res.send(compiledFile({ title: "Mental Health Articles", heading: "Mental Health Articles" }));
+	res.send(compiledFile({ title: "Mental Health Articles" }));
 });
 Router.get("/mental-illness-definitions",(req,res) => {
 	const compiledFile = pug.compileFile("./views/mental-illness-definitions.pug");
 
-	res.send(compiledFile({ title: "Mental Illness Directory", heading: "Mental Illness Directory" }));
+	res.send(compiledFile({ title: "Mental Illness Directory" }));
 });
 Router.get("/support",(req,res) => {
 	const compiledFile = pug.compileFile("./views/support.pug");
 
-	res.send(compiledFile({ title: "Support", heading: "Mental Health Support" }));
+	res.send(compiledFile({ title: "Support" }));
 })
 
 module.exports = Router;
